64d94763e454c2486d1d5d1f2f53b7f2b6b38e9d
Julia Lindman <julialindmanmusic@gmail.com>
2022-06-02 20:26:59 +0200

    Initial commit

---
+++ src/main/java/org/example/Five.java
@@ -0,0 +1,11 @@
+package org.example;
+import java.util.*;
+
+public class Five {
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("What's your name? ");
+        String name= sc.nextLine();
+        System.out.print("Hello "+name);
+    }
+}
